49e3618ff4325039f2f2649d844c406476bc9187
MatheusLBarbosa <barbosa.matheuslopes@gmail.com>
2023-03-11 09:00:18 -0300

    project hr-payroll created

---
+++ hr-payroll/src/test/java/com/mlb/hrpayroll/HrPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mlb.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
