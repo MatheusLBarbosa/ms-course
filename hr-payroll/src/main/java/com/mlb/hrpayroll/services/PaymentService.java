806ca291e2edaa3fe3dbcd3ac3f75d8f73274b66
MatheusLBarbosa <barbosa.matheuslopes@gmail.com>
2023-03-11 09:29:08 -0300

    hr-payroll implementation mock

---
+++ hr-payroll/src/main/java/com/mlb/hrpayroll/services/PaymentService.java
@@ -0,0 +1,14 @@
+package com.mlb.hrpayroll.services;
+
+import org.springframework.stereotype.Service;
+
+import com.mlb.hrpayroll.entities.Payment;
+
+@Service
+public class PaymentService {
+
+	public Payment getPayment(long workerId, int days) {
+		return new Payment("Bob", 200.0, days);
+	}
+	
+}
