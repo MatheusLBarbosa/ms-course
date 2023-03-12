657e4b2249075f5b999b669072c8b14b863bbdc9
MatheusLBarbosa <barbosa.matheuslopes@gmail.com>
2023-03-12 08:44:36 -0300

    Configuring Eureka Client

--- hr-worker/src/main/java/com/mlb/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/mlb/hrworker/resources/WorkerResource.java
@@ -1,21 +1,29 @@
 package com.mlb.hrworker.resources;
 
-import com.mlb.hrworker.entities.Worker;
-import com.mlb.hrworker.repositories.WorkerRepository;
-import org.hibernate.jdbc.Work;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.mlb.hrworker.entities.Worker;
+import com.mlb.hrworker.repositories.WorkerRepository;
 
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
+	
+	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 
+	@Autowired
+	private Environment env;
+	
     @Autowired
     private WorkerRepository repository;
 
@@ -28,8 +36,9 @@ public class WorkerResource {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
+    	logger.info("PORT = " + env.getProperty("local.server.port"));
+    	
         Worker worker = repository.findById(id).get();
-
         return ResponseEntity.ok(worker);
     }
 }
